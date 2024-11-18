69b74b08b9e78ebc3d3486b63ffbb16021d79417
Aryan16032004 <aryan16062004@gmail.com>
2024-11-18 11:26:43 +0530

    css

--- Frontend/src/components/home/hero/Hero.jsx
+++ Frontend/src/components/home/hero/Hero.jsx
@@ -4,12 +4,13 @@ import "./Hero.css"
 
 const Hero = () => {
   return (
-    <div className="text-teal-400">
+    <div className="">
       <section className='hero text-teal-400'>
+      <img src='./images/bg.webp' alt='' />
         <div className='container'>
           <div className='row'>
             <Heading subtitle='Dr.Maths Institute ' title='Best Online Education Expertise' />
-            <p className="bg-black">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <p className="">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
             <div className='button'>
               <button className='primary-btn'>
                 GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
